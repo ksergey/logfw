5c33cbb3a4a1f2a5f3f00cf403587753432e75b0
Sergey Kovalevich <inndie@gmail.com>
2016-12-21 01:06:52 +0300

    new funcs

--- examples/test1.cpp
+++ examples/test1.cpp
@@ -6,25 +6,107 @@
 #include <array>
 #include <utility>
 
+template< std::size_t N >
+using const_string = std::array< const char, N >;
+
+template< std::size_t N, std::size_t... I >
+constexpr const_string< N > to_const_string(char const (&a)[N], std::index_sequence< I... >)
+{
+    return {{ a[I]... }};
+}
+
+template< std::size_t N >
+constexpr const_string< N > to_const_string(char const (&a)[N])
+{
+    return to_const_string(a, std::make_index_sequence< N >{});
+}
+
+template< std::size_t N >
+constexpr const_string< N > to_const_string(const const_string< N >& a)
+{
+    return a;
+}
+
+constexpr const_string< 2 > to_const_string(char ch)
+{
+    return {{ ch, '\0' }};
+}
+
+#if 0
 template< std::size_t N1, std::size_t... I1, std::size_t N2, std::size_t... I2 >
-constexpr std::array< char const, N1 + N2 - 1 >
-concat(char const (&a1)[N1], char const (&a2)[N2], std::index_sequence< I1... >, std::index_sequence< I2... >)
+constexpr const_string< N1 + N2 - 1 >
+concat(char const (&a1)[N1], char const (&a2)[N2],
+        std::index_sequence< I1... >, std::index_sequence< I2... >)
 {
     return {{ a1[I1]..., a2[I2]... }};
 }
 
-template< std::size_t N1, std::size_t N2>
-constexpr std::array< char const, N1 + N2 - 1 >
+template< std::size_t N1, std::size_t N2 >
+constexpr const_string< N1 + N2 - 1 >
 concat(char const (&a1)[N1], char const (&a2)[N2])
 {
     return concat(a1, a2, std::make_index_sequence< N1 - 1 >{}, std::make_index_sequence< N2 >{});
 }
 
-constexpr const auto cc = concat("=====================hello", " world################3");
+template< std::size_t N, std::size_t... I >
+constexpr const_string< N + 1 >
+concat(char const (&a)[N], char ch, std::index_sequence< I... >)
+{
+    return {{ a[I]..., ch, '\0' }};
+}
+
+template< std::size_t N >
+constexpr const_string< N + 1 >
+concat(char const (&a)[N], char ch)
+{
+    return concat(a, ch, std::make_index_sequence< N - 1 >{});
+}
+#endif
+
+template< std::size_t N1, std::size_t... I1, std::size_t N2, std::size_t... I2 >
+constexpr const_string< N1 + N2 - 1 >
+concat(const const_string< N1 >& a1, const const_string< N2 >& a2,
+        std::index_sequence< I1... >, std::index_sequence< I2... >)
+{
+    return {{ a1[I1]..., a2[I2]... }};
+}
+
+template< std::size_t N1, std::size_t N2 >
+constexpr const_string< N1 + N2 - 1 >
+concat(const const_string< N1 >& a1, const const_string< N2 >& a2)
+{
+    return concat(a1, a2, std::make_index_sequence< N1 - 1 >{}, std::make_index_sequence< N2 >{});
+}
+
+template< class Arg0, class Arg1 >
+constexpr auto concat(const Arg0& a0, const Arg1& a1)
+{
+    return concat(to_const_string(a0), to_const_string(a1));
+}
+
+constexpr const_string< 1 > concat()
+{
+    return {{ '\0' }};
+}
+
+template< class Arg0, class... Args >
+constexpr auto concat(const Arg0& a0, const Args&... args)
+{
+    return concat(a0, concat(args...));
+}
+
+constexpr const auto cc0 = concat("=====================hello", " world################3");
+constexpr const auto cc1 = concat("xxxxxx ", 'y');
+constexpr const auto cc2 = concat(cc0, cc1);
+constexpr const auto cc3 = concat("hello", ' ', "world", '!');
+constexpr const auto cc4 = concat("hello", ' ', "world", ' ', cc1);
 
 int main()
 {
-    std::cout << cc.data() << '\n';
-    std::cout << "testggggggggggggggggggggggggggggggggg\n";
+    std::cout << cc0.data() << '\n';
+    std::cout << cc1.data() << '\n';
+    std::cout << cc2.data() << '\n';
+    std::cout << cc3.data() << '\n';
+    std::cout << cc4.data() << '\n';
     return 0;
 }
